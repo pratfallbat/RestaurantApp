db34dd7497616157b3f39cc8e3352125a67206d2
Pratyush kumar <pratyush845@gmail.com>
2017-07-16 01:15:38 +0530

    functionality for view added

--- app/controllers/menu_controller.rb
+++ app/controllers/menu_controller.rb
@@ -1,4 +1,5 @@
 class MenuController < ApplicationController
   def index
+  	@food_items = FoodItem.all
   end
 end
--- app/models/food_item.rb
+++ app/models/food_item.rb
@@ -1,2 +1,9 @@
 class FoodItem < ApplicationRecord
+	def image_url_or_default
+		if image_url.present?
+			image_url
+		else
+			"http://loremflickr.com/320/240/#{name}"
+		end
+	end
 end
