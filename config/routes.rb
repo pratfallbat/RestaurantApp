32b037a24c845bb6f19d9d2ab3ee11819e8f9aaa
Pratyush kumar <pratyush845@gmail.com>
2017-07-15 21:35:55 +0530

    added first view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
- get 'contact_us' => 'welcome#contact_us'
+  get 'menu' =>'menu#index'
+
+ get '/contact_us'  => 'welcome#contact_us'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root 'welcome#index'
